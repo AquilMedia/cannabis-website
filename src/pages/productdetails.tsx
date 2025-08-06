31ae57564557941ae285515e69ffd440eb69600f
Akshay Bhujbal <akshaybhujbal00@gmail.com>
2025-08-06 16:54:45 +0530

    PRODUCT PAGE

---
+++ src/pages/productdetails.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Productdetails: React.FC = () => {
+    return (
+        <div className="privacy-policy">
+            <h1>       Products details </h1>
+
+
+
+        </div>
+    );
+};
+
+export default Productdetails;
---
+++ src/pages/productslisting.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Productslisting: React.FC = () => {
+    return (
+        <div className="privacy-policy">
+            <h1>       Products listing </h1>
+
+                                <a className="nav-link f-size-18 f-w-M" href="/productdetails">Product Details</a>
+
+        </div>
+    );
+};
+
+export default Productslisting;
